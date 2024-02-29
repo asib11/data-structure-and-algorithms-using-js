666478f046bf13fad942525e852cc131441fcc94
asib ahmed <ahmedasib1@gmail.com>
2024-03-01 00:27:45 +0600

    linear search

--- searching/linear-search.js
+++ searching/linear-search.js
@@ -1,8 +1,8 @@
-const linearSearch = (arr, value) =>{
-    for(let i=0; i<arr.length; i++){
-        if (arr[i]===value) return i;
+const linearSearch = (arr, value) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) return i;
     }
     return -1;
 }
 
-console.log(linearSearch([18,5,3,24,65,33], 33))
+console.log(linearSearch([18, 5, 3, 24, 65, 33], 33))
