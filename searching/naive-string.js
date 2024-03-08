0599f328d3a27bf587eb048efd73e553f41bd1f6
asib ahmed <ahmedasib1@gmail.com>
2024-03-08 18:34:51 +0600

    naive search string

---
+++ searching/naive-string.js
@@ -0,0 +1,12 @@
+const naiveString = (long, pattern) =>{
+    let count = 0
+    for (let i = 0; i< long.length ; i++){
+        for(let j = 0; j< pattern.length ; j++){
+            if(pattern[j] !== long[i+j]) break;
+            if(j === pattern.length -1) count++;
+        }
+    }
+    return count;
+}
+
+console.log(naiveString("asib ah ahmed", "ao"))
