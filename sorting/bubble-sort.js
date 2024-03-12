d15ff63f3fc0bcf962cc86700fbfc4c4e56194bb
asib ahmed <ahmedasib1@gmail.com>
2024-03-13 00:14:45 +0600

    add js version of bubble sort

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -1,7 +1,15 @@
 const bubbleSort =arr =>{
     for (let i = arr.length; i>0 ; i--){
-        for (j = 0 ; j < i.length)
+        for (j = 0 ; j < i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                // swap
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+            }
+        }
     }
+    return arr;
 }
 
-console.log(bubbleSort([32,12,45,21,6]))
+console.log(bubbleSort([32,12,45,21,6,-3]))
