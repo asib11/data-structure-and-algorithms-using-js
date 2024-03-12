0171a9d594fd5b7e6a48d414744e8ea048b1fb9d
asib ahmed <ahmedasib1@gmail.com>
2024-03-12 23:57:12 +0600

    bubble sort

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -1,3 +1,7 @@
-const bubbleSort =( arr) =>{
-
+const bubbleSort =arr =>{
+    for (let i = arr.length; i>0 ; i--){
+        for (j = 0 ; j < i.length)
+    }
 }
+
+console.log(bubbleSort([32,12,45,21,6]))
