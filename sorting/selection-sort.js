7ca8af9e65e414d0b0c5e200c4345757e26101ce
asib ahmed <ahmedasib1@gmail.com>
2024-03-18 00:06:27 +0600

    selection sort es6 version

--- sorting/selection-sort.js
+++ sorting/selection-sort.js
@@ -1,14 +1,28 @@
+// const selectionSort = arr => {
+//     for (let i = 0; i < arr.length; i++) {
+//         let lowest = i;
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (arr[j] < arr[lowest]) lowest = j;
+//         }
+//         if (i !== lowest) {
+//             let temp = arr[i];
+//             arr[i] = arr[lowest];
+//             arr[lowest] = temp;
+//         }
+//     }
+//     return arr;
+// }
+
 const selectionSort = arr => {
+    const swap =(arr, indx1, indx2) =>{
+        return [arr[indx1], arr[indx2]] = [arr[indx2], arr[indx1]]
+    }
     for (let i = 0; i < arr.length; i++) {
         let lowest = i;
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[j] < arr[lowest]) lowest = j;
         }
-        if (i !== lowest) {
-            let temp = arr[i];
-            arr[i] = arr[lowest];
-            arr[lowest] = temp;
-        }
+        if (i !== lowest) swap(arr, i, lowest)
     }
     return arr;
 }
