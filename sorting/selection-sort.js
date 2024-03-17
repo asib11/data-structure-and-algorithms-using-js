320fec760befb9a3e177bacf7e560f96b327ef2a
asib ahmed <ahmedasib1@gmail.com>
2024-03-17 23:56:17 +0600

    selection sort

--- sorting/selection-sort.js
+++ sorting/selection-sort.js
@@ -1,8 +1,14 @@
 const selectionSort = arr =>{
     for (let i= 0; i < arr.length; i++){
-        
+        let lowest = i;
+        for (let j = i+1 ; j < arr.length; j++){
+            if(arr[j]< arr[lowest]) lowest = j;
+        }
+        let temp = arr[i];
+        arr[i] = arr[lowest];
+        arr[lowest] = temp;
     }
-
+    return arr;
 }
 
 console.log(selectionSort([23,43,32, -3, 15]))
