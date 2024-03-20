cf015abf129b3e4e7ce647bbb91b18a6b438211e
asib ahmed <ahmedasib1@gmail.com>
2024-03-21 00:20:12 +0600

    insertion sort

--- sorting/insertion.js
+++ sorting/insertion.js
@@ -1,4 +1,11 @@
 const insertionSort = arr =>{
+    for (let i = 1; i< arr.length; i++){
+        let currentValue = arr[i];
+        for (var j = i-1; j>=0 && arr[j] > currentValue; j--){
+            arr[j+1] = arr[j];
+        }
+        arr[j+1] = currentValue;
+    }
     return arr
 }
 
