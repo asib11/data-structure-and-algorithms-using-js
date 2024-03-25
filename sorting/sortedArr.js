aae375668f80f4f4be067b77d57f8deaf67deb1f
asib ahmed <ahmedasib1@gmail.com>
2024-03-25 23:55:58 +0600

    marge two sorted array

--- sorting/sortedArr.js
+++ sorting/sortedArr.js
@@ -1,9 +1,27 @@
 const sortedArrary = (arr1, arr2) =>{
     const arr3 = [];
-    for(let i =0 ; i < arr1.length; i++){
-        
+    let i = 0
+    let j = 0
+    while(i< arr1.length && j< arr2.length){
+        if(arr2[j]> arr1[i]){
+            arr3.push(arr1[i]);
+            i++;
+        } else{
+            arr3.push(arr2[j]);
+            j++
+        }
     }
 
+    while(i < arr1.length){
+        arr3.push(arr1[i]);
+        i++;
+    }
+
+    while(j < arr2.length){
+        arr3.push(arr2[j]);
+        j++;
+    }
+    return arr3
 }
 
-console.log(sortedArrary([2,3,6,9],[1,5,7,8]))
+console.log(sortedArrary([2,3,6],[1,5,7,8]))
