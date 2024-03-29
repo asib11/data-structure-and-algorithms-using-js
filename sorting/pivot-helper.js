2c0010b521a08ab65832a001f828dc9adc91e797
asib ahmed <ahmedasib1@gmail.com>
2024-03-29 19:48:46 +0600

    pivot

--- sorting/pivot-helper.js
+++ sorting/pivot-helper.js
@@ -1,6 +1,19 @@
-const pivot = (arr, start, end) =>{
+const pivot = (arr, start=0, end = arr.length + 1) =>{
+    const swap = (arr, i, j)=>{
+        return [arr[i], arr[j]] = [arr[j], arr[i]]
+    }
 
-    return arr
+    let pivot = arr[start];
+    let swapIdx = start;
+    for (let i = start+1; i< arr.length; i++){
+        if(pivot > arr[i]){
+            swapIdx++;
+            swap(arr, swapIdx, i)
+        }
+    }
+    swap(arr, start, swapIdx)
+
+    return swapIdx
 }
 
 console.log(pivot([28,5,3,24,6,4]))
