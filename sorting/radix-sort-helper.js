054a4279386e643b984c16a6d6d49bbf93e44272
asib ahmed <ahmedasib1@gmail.com>
2024-04-01 19:29:29 +0600

    digit count

--- sorting/radix-sort-helper.js
+++ sorting/radix-sort-helper.js
@@ -2,5 +2,10 @@ const getDigit = (num,i) =>{
     return Math.floor(Math.abs(num)/ Math.pow(10,i))%10;
 }
 
+const digitCount = num => {
+    if( num === 0) return 1; // because log10(0) gets -infinity
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
 
 console.log(getDigit(1234,2))
+console.log(digitCount(1234))
