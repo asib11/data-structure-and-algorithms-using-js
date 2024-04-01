9a432473e60c575bbd77f47a3a5a9827b5f581a4
asib ahmed <ahmedasib1@gmail.com>
2024-04-01 19:35:10 +0600

    most digit

--- sorting/radix-sort-helper.js
+++ sorting/radix-sort-helper.js
@@ -7,5 +7,14 @@ const digitCount = num => {
     return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
 
+const mostDigit = nums =>{
+    let maxtDigit = 0;
+    for(let i = 0; i < nums.length ; i++){
+        maxtDigit = Math.max(maxtDigit, digitCount(nums[i]))
+    }
+    return maxtDigit;
+}
+
 console.log(getDigit(1234,2))
 console.log(digitCount(1234))
+console.log(mostDigit([2,54,675,44,644446,33]))
