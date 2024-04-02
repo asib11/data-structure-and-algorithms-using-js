d549bf1584fe3760f3469c71d4e51cd155a394d8
asib ahmed <ahmedasib1@gmail.com>
2024-04-02 16:44:46 +0600

    radix sort

---
+++ sorting/radix-sort.js
@@ -0,0 +1,33 @@
+const getDigit = (num,i) =>{
+    return Math.floor(Math.abs(num)/ Math.pow(10,i))%10;
+}
+
+const digitCount = num => {
+    if( num === 0) return 1; // because log10(0) gets -infinity
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+const mostDigit = nums =>{
+    let maxtDigit = 0;
+    for(let i = 0; i < nums.length ; i++){
+        maxtDigit = Math.max(maxtDigit, digitCount(nums[i]))
+    }
+    return maxtDigit;
+}
+
+const radixSort = nums =>{
+    let maxDigitCount = mostDigit(nums);
+    for(k = 0; k< maxDigitCount; k++){
+        let digitBucket = Array.from({length: 10}, ()=> []);
+        for(let i = 0; i <nums.length; i++){
+            digitBucket[getDigit(nums[i], k)].push(nums[i]);
+        }
+        nums = [].concat(...digitBucket);
+    }
+    return nums
+}
+
+// console.log(getDigit(1234,2))
+// console.log(digitCount(1234))
+// console.log(mostDigit([2,54,675,44,644446,33]))
+console.log(radixSort([2,4254,675,44,644446,33]))
