5ece91ca850ee587c244c20ae98ba39670a45ca2
asib ahmed <ahmedasib1@gmail.com>
2024-04-08 16:17:38 +0600

    doubly link list push

--- doubly-link-list/doubly-link-list.js
+++ doubly-link-list/doubly-link-list.js
@@ -12,4 +12,24 @@ class DoublyLinkList{
         this.tail = null;
         this.length = 0;
     }
+
+    push(val){
+        let newNode = new Node(val);
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode // update new tail
+        }
+        this.length++;
+        return this;
+    }
 }
+
+let list = new DoublyLinkList();
+list.push(10)
+list.push(20)
+list.push(30)
+console.log(list)
