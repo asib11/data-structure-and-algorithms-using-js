3383ce85234bb5b6ab8b2d9e4bb69b3f7984bab5
asib ahmed <ahmedasib1@gmail.com>
2024-04-15 22:45:23 +0600

    queue adding data

---
+++ stack-queue/queue.js
@@ -0,0 +1,27 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Queue{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0
+    }
+
+    enqueue(val){ // enqueue = push = add data from last
+        let newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        }else{
+            this.last.next = newNode
+            this.last = newNode
+        }
+        return ++this.size
+    }
+}
+
