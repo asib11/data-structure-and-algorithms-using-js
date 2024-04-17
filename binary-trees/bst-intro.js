253f86b6e572743afa1c77db312f1f46bcfc4ba5
asib ahmed <ahmedasib1@gmail.com>
2024-04-17 23:47:43 +0600

    bst node and root make

---
+++ binary-trees/bst-intro.js
@@ -0,0 +1,18 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null
+    }
+}
+
+let tree = new BinarySearchTree();
+console.log(tree.root = new Node(10))
+console.log(tree.root.left = new Node(8))
+console.log(tree.root.right = new Node(15))
