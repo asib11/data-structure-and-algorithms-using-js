ebe93203187f0afbb970c11ca864d62fa0fb7724
asib ahmed <ahmedasib1@gmail.com>
2024-04-19 23:38:16 +0600

    fix: insert node

--- binary-trees/bst-intro.js
+++ binary-trees/bst-intro.js
@@ -22,13 +22,13 @@ class BinarySearchTree{
             if(this.root === value) return undefined;
             if(value < current.value){
                 if(current.left === null){
-                    this.left = newNode;
+                    current.left = newNode;
                     return this;
                 }
                 current = current.left;
-            }else {
-                if(value > current.value){
-                    this.right = newNode;
+            }else if (value > current.value){
+                if(current.right === null){
+                    current.right = newNode;
                     return this;
                 }
                 current = current.right;
@@ -39,6 +39,14 @@ class BinarySearchTree{
 }
 
 let tree = new BinarySearchTree();
-console.log(tree.root = new Node(10))
-console.log(tree.root.left = new Node(8))
-console.log(tree.root.right = new Node(15))
+// console.log(tree.root = new Node(10))
+// console.log(tree.root.left = new Node(8))
+// console.log(tree.root.right = new Node(15))
+tree.insert(10)
+tree.insert(8)
+tree.insert(9)
+tree.insert(3)
+tree.insert(13)
+tree.insert(12)
+console.log(tree)
+// console.log(tree.root.right)
