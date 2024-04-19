bdde570a86e73f0bd07a4e7bdd11a0ec4ee2047d
asib ahmed <ahmedasib1@gmail.com>
2024-04-19 22:36:41 +0600

    bst insert

--- binary-trees/bst-intro.js
+++ binary-trees/bst-intro.js
@@ -17,6 +17,24 @@ class BinarySearchTree{
             this.root = newNode;
             return this;
         }
+        let current = this.root;
+        while(true){
+            if(this.root === value) return undefined;
+            if(value < current.value){
+                if(current.left === null){
+                    this.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            }else {
+                if(value > current.value){
+                    this.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+
     }
 }
 
