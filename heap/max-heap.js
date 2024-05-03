37ed3e37a2a4008df42f39828c783ae8a4136a25
asib ahmed <ahmedasib1@gmail.com>
2024-05-03 19:19:38 +0600

    feat: removing heap

--- heap/max-heap.js
+++ heap/max-heap.js
@@ -3,6 +3,15 @@ class MaxBinaryHeap{
         this.values = [41,39,33,18,27,12];
     }
 
+//     INSERT PSEUDOCODE
+// Push the value into the values property on the heap
+// Bubble Up:
+// Create a variable called index which is the length of the values property - 1
+// Create a variable called parentIndex which is the floor of (index-1)/2
+// Keep looping as long as the values element at the parentIndex is less than the values element at the child index
+// Swap the value of the values element at the parentIndex with the value of the element property at the child index
+// Set the index to be the parentIndex, and start over!
+
     insert(element){
         this.values.push(element);
         this.bubbleUp()
@@ -20,8 +29,62 @@ class MaxBinaryHeap{
             idx = parentIdx;
         }
     }
+
+//     REMOVING
+// Swap the first value in the values property with the last one
+// Pop from the values property, so you can return the value at the end.
+// Have the new root "sink down" to the correct spot...​
+// Your parent index starts at 0 (the root)
+// Find the index of the left child: 2 * index + 1 (make sure its not out of bounds)
+// Find the index of the right child: 2*index + 2 (make sure its not out of bounds)
+// If the left or right child is greater than the element...swap. If both left and right children are larger, swap with the largest child.
+// The child index you swapped to now becomes the new parent index.  
+// Keep looping and swapping until neither child is larger than the element.
+// Return the old root!
+
+    extractMax(){
+        const max = this.values[0];
+        const end = this.values.pop()
+        if(this.values.length > 0){
+            this.values[0] = end;
+            this.sinkDown()
+        }
+        return max;
+    }
+    sinkDown(){
+        let idx = 0; // parent index
+        const length = this.values.length;
+        const element = this.values[0];
+        while(true){
+            let leftChildIdx = 2*idx +1;
+            let rightChildIdx = 2* idx + 2;
+            let leftChild, rightChild, swap = null;
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx];
+                if(leftChild > element) swap = leftChildIdx;
+            }
+            if(rightChildIdx < length){
+                rightChild = this.values[rightChildIdx]
+                if((swap === null && rightChild < element) || (swap !== null && rightChild > leftChild)) swap = rightChildIdx;
+            }
+            if(swap === null) break;
+            this.values[idx] = this.values[swap]; // set child index to parent index
+            this.values[swap] = element;
+            idx = swap;
+        }
+    }
 }
 
 let heap = new MaxBinaryHeap();
 heap.insert(55)
+console.log(heap.values)
+console.log(heap.extractMax())
+console.log(heap.extractMax())
+console.log(heap.extractMax())
+console.log(heap.extractMax())
+console.log(heap.extractMax())
+console.log(heap.values)
+console.log(heap.extractMax())
+console.log(heap.extractMax())
+console.log(heap.extractMax())
 console.log(heap.values)
