5b491ef277af0d93a3e2974445747df25997b8d2
asib ahmed <ahmedasib1@gmail.com>
2024-05-17 17:50:50 +0600

    feat: remove edge method

--- graph/graph.js
+++ graph/graph.js
@@ -11,6 +11,11 @@ class Graph{
         this.adjacencyList[v1].push(v2)
         this.adjacencyList[v2].push(v1)
     }
+
+    removeEdge(v1,v2){
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2)
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1)
+    }
 }
 
 let g = new Graph()
@@ -21,4 +26,6 @@ console.log(g)
 g.addEdge('asib', 'ahmed')
 g.addEdge('asib', 'tanveer')
 g.addEdge('ahmed', 'tanveer')
+console.log(g)
+g.removeEdge('ahmed', 'tanveer')
 console.log(g)
